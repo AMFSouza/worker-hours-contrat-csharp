e26fa2714156db48ad21ab6796a16a419f06bfb3
AMFSouza <amfdeveloper@gmail.com>
2021-10-14 03:19:00 -0300

    HourContract class

---
+++ src/HourContracts/Entities/HourContract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HourContracts.Entities
+{
+    class HourContract
+    {
+        public DateTime date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+        public HourContract() { }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            this.date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}
