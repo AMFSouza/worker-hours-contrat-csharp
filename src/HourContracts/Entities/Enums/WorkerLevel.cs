7ef76e8cb4d19b62aede89b170a2facaee9f7cfc
AMFSouza <amfdeveloper@gmail.com>
2021-10-14 02:59:05 -0300

    WorkerLevel enumeration

---
+++ src/HourContracts/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HourContracts.Entities.Enums
+{
+    enum WorkerLevel
+    {
+        Junior = 1,
+        MidLevel = 2,
+        Senior = 3
+
+    }
+}
